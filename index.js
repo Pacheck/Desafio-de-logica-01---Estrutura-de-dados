92b9ad2b4beaf2db6c703695f209b76a6e930f8d
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-10 20:09:46 -0300

    finished

---
+++ index.js
@@ -0,0 +1,15 @@
+const nome = "Pedro Pacheco";
+
+const idade = 24;
+
+const anoDeNascimento = 2021 - idade;
+
+console.log('Meu ano de nascimento é ' + anoDeNascimento);
+
+const souHumano = false;
+
+const a = 1;
+const b = 2;
+
+console.log(a + b);
+
